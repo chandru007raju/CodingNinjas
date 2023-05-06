a36522b8d023eb4e71bb0bb8e80fc058c1319178
sr.chandrashekar <chandru007.raju@gmail.com>
2023-05-06 15:25:45 +0530

    Updated Changes

---
+++ Introduction-To-C++/l-8_Arrays2_SearchingAndSorting/Assignments/CheckArrayRotation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int CheckArrayRotation(int arr[],int s){
+    int count = 0;
+    for (int i=0;i<s;i++){
+        if(arr[i] < arr[i+1]){
+            count++;
+        }else{
+            return count+1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int s;
+        cin>>s;
+
+        int* arr = new int[s];
+        for(int i=0;i<s;i++){
+            cin>>arr[i];
+        }
+        cout<<CheckArrayRotation(arr,s);
+        cout<<endl;
+        delete[]arr;
+
+    }
+}
