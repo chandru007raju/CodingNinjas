8de9c5e541250ed0b13a05d9ced11ace46feed88
sr.chandrashekar <chandru007.raju@gmail.com>
2023-05-11 20:41:14 +0530

    Updated Changes

--- DSA using C++/Pointers/simpleExampleChecker.cpp
+++ DSA using C++/Pointers/simpleExampleChecker.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main(){
+
     // int a = 7;
     // int *c = &a;
-    // // c = c + 1;
-    // cout  << a << "  " << *c << endl;
+    // c = c + 1;
+    // cout  << a << "  " << *c <<endl;
 
 
-//     int a[3] = {2, 3};
-// cout << *(a)<<" " <<a <<" "<<a[0];
+    // int a[3] = {2, 3};
+    // cout << *(a)<<" " <<a <<" "<<a[0];
 
     // int a[6] = {1, 2, 3};
     //     int *b = a;
     //     cout << b[0];
+    
 }
