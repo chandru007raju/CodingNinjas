dc26674c10e2d4b1a83bb0e56935b1ad500bf771
sr.chandrashekar <chandru007.raju@gmail.com>
2023-05-29 13:05:04 +0530

    Updated Changes

--- Introduction-To-C++/L-8_Arrays1_/Exercises/ChangeNumbersInArray.cpp
+++ Introduction-To-C++/L-8_Arrays1_/Exercises/ChangeNumbersInArray.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 
+//  THIS Program tells that we NEED NOT RETURN THE Array back from a Function -->
+
+// Because the change is reflected directly into the array as we used the address in the function to make changes to the Array;
+
 void changeArray(int arr[], int n){
     for(int i=0;i<n;i++){
         arr[i]++;
@@ -13,7 +17,13 @@ void printArray(int arr[], int n){
     }
 
 }
-
+void Reverse(int arr[],int n){
+    for(int i=0,j=n-1;i<=j;i++,j--){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
 int main(){
     int n;
     cin>>n;
@@ -23,5 +33,6 @@ int main(){
         cin>>arr[i];
     }
     changeArray(arr,n);
+    // Reverse(arr,n);
     printArray(arr,n);
 }
