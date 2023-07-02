9f8c0006541751ef47e44e73e70da19e73531779
sr.chandrashekar <chandru007.raju@gmail.com>
2023-07-02 22:16:52 +0530

    updated Changes

---
+++ DSA using C++/Lecture-15-Priority-Queues/Code/priorityQueue.h
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+class PriorityQueue{
+    private:
+    vector <int> pq;
+
+    public:
+    PriorityQueue(){        
+    }
+
+    bool isEmpty(){
+        return (pq.size() == 0);
+    }
+
+    
+
+
+};
