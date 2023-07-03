7475c1e36cbdf4dd0f31bd4a77bb4887d3051c8d
sr.chandrashekar <chandru007.raju@gmail.com>
2023-07-03 20:28:22 +0530

    Updated Changes

--- DSA using C++/Lecture-15-Priority-Queues/Code/MypriorityQueue.h
+++ DSA using C++/Lecture-15-Priority-Queues/Code/MypriorityQueue.h
@@ -58,8 +58,8 @@ public:
 
     */
 
-    int MyremoveMin()
-    { // -->1 -->2 -->3
+    int MyremoveMin() // My extra Function -->
+    {                 // -->1 -->2 -->3
         if (pq.empty())
         {
             return 0;
@@ -141,6 +141,105 @@ public:
             pq[parentIndex] = temp;
 
             parentIndex = minIndex;
+            LeftChildIndex = 2 * parentIndex + 1;
+            RightChildIndex = 2 * parentIndex + 2;
+        }
+        return ans;
+    }
+};
+
+
+//   Now Lets Implement the { Max Heap } ---->
+
+class MaxPQ
+{
+private:
+    vector<int> maxpq;
+
+public:
+    MaxPQ()
+    {
+    }
+
+    bool isEmpty()
+    {
+        return maxpq.size() == 0;
+    }
+
+    int GetMax()
+    {
+        if (maxpq.empty())
+        {
+            return 0;
+        }
+        return maxpq[0];
+    }
+
+    int GetSize()
+    {
+        return maxpq.size();
+    }
+
+    int insert(int element) // 1 2 3 1000
+    {
+        maxpq.push_back(element);
+
+        int childIndex = maxpq.size() - 1;
+        while (childIndex > 0)
+        {
+
+            int parentIndex = (childIndex - 1) / 2;
+
+            if (maxpq[parentIndex] < maxpq[childIndex])
+            {
+                int temp = maxpq[childIndex];
+                maxpq[childIndex] = maxpq[parentIndex];
+                maxpq[parentIndex] = temp;
+
+            }
+            else
+            {
+                break;
+            }
+            childIndex = parentIndex;
+        }
+    }
+
+    int removeMax()
+    {
+        if (maxpq.empty())
+        {
+            return 0;
+        }
+        int ans = maxpq[0];
+        maxpq[0] = maxpq[maxpq.size() - 1];
+        maxpq.pop_back();
+
+        int parentIndex = 0;
+        int LeftChildIndex = 2 * parentIndex + 1;
+        int RightChildIndex = 2 * parentIndex + 2;
+
+        while (LeftChildIndex < maxpq.size())
+        {
+            int maxIndex = parentIndex;
+
+            if (maxpq[LeftChildIndex] > maxpq[maxIndex])
+            {
+                maxIndex = LeftChildIndex;
+            }
+            if (RightChildIndex < maxpq.size() && maxpq[RightChildIndex] > maxpq[maxIndex])
+            {
+                maxIndex = RightChildIndex;
+            }
+            if (maxIndex == parentIndex)
+            {
+                break;
+            }
+            int temp = maxpq[maxIndex];
+            maxpq[maxIndex] = maxpq[parentIndex];
+            maxpq[parentIndex] = temp;
+
+            parentIndex = maxIndex;
             LeftChildIndex = 2 * parentIndex + 1;
             RightChildIndex = 2 * parentIndex + 2;
         }
--- DSA using C++/Lecture-15-Priority-Queues/Code/mypq-Use.cpp
+++ DSA using C++/Lecture-15-Priority-Queues/Code/mypq-Use.cpp
@@ -1,44 +1,84 @@
 #include<iostream>
-#include "MypriorityQueue.h"
+#include "maxpPriorityQueue.h"
+// #include "MypriorityQueue.h"
 
 using namespace std;
 
+
+// int main()
+// {
+//     MyPriorityQueue p;
+//     /*
+//     // One Set Of Example -->
+//     p.insert(100);
+//     p.insert(10);
+//     p.insert(11);
+//     p.insert(30);
+//     p.insert(7);
+//     p.insert(80);
+//     p.insert(2);
+//     p.insert(30);
+
+//     */
+//     /* 2nd Set Of Example --> */
+
+//     p.insert(100);
+//     p.insert(10); //  --> 2 nd
+//     p.insert(15); 
+//     p.insert(4);  // ---> 1 st
+//     p.insert(17);
+//     p.insert(21);
+//     p.insert(67);
+
+//     cout<<p.GetSize()<<" -> size"<<endl;
+//     cout<<p.MyremoveMin()<<endl;
+//     cout<<p.GetSize()<<" -> size"<<endl;
+
+//     cout<<p.GetMin()<<" --> Get Min "<<endl;
+
+//     while(!p.isEmpty()){
+//         cout<<p.removeMin()<<" ";
+//     }
+
+//     return 0;
+
+// }
+
+
+
+//  FOR THE maxpPriorityQueue folder -->
+
 int main()
 {
-    MyPriorityQueue p;
-    /*
-    // One Set Of Example -->
-    p.insert(100);
-    p.insert(10);
-    p.insert(11);
-    p.insert(30);
-    p.insert(7);
-    p.insert(80);
-    p.insert(2);
-    p.insert(30);
+    PriorityQueue mp; // Max Priority Queue -->
+    mp.insert(10);
+    mp.insert(100);
+    mp.insert(15);
+    mp.insert(110); // --> 2 nd
+    // mp.insert(60);
+    // mp.insert(45);
+    // mp.insert(1000); // --> 1 st
 
-    */
 
-    // 2nd Set Of Example -->
+    cout<<" Size ->" <<mp.getSize()<<endl;
 
-    p.insert(100);
-    p.insert(10); //  --> 2 nd
-    p.insert(15); 
-    p.insert(4);  // ---> 1 st
-    p.insert(17);
-    p.insert(21);
-    p.insert(67);
+    cout<<" Get Max ->" <<mp.getMax()<<endl;
 
-    cout<<p.GetSize()<<" -> size"<<endl;
-    cout<<p.MyremoveMin()<<endl;
-    cout<<p.GetSize()<<" -> size"<<endl;
+    cout<<" Remove Max element ->" <<mp.removeMax()<<endl;
 
-    cout<<p.GetMin()<<" --> Get Min "<<endl;
+    cout<<" Size ->" <<mp.getSize()<<endl;
 
-    while(!p.isEmpty()){
-        cout<<p.removeMin()<<" ";
+    while(! mp.isEmpty()){
+        cout<<mp.removeMax()<<" ";
     }
 
     return 0;
-
 }
+
+    // 10
+    // 100 10
+    // 100 10 15 -> 100 10 15 [110] swap with index 1 --> 
+    // 100  [110] 15 10  swap with index 0 -->
+    // 110 100 15 10 -----------------> insert(60)  
+
+
