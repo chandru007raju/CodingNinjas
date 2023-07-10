52c6f34c58363773ee2009b76c2ba40eb2e7b268
sr.chandrashekar <chandru007.raju@gmail.com>
2023-07-10 18:43:48 +0530

    Updated Changes

--- DSA using C++/Lecture-16-Tries-and-Huffman-Coding/Assignment/pattern-matching.cpp
+++ DSA using C++/Lecture-16-Tries-and-Huffman-Coding/Assignment/pattern-matching.cpp
@@ -38,10 +38,12 @@ false
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
 
 class TrieNode
 {
+
 public:
     char data;
     TrieNode **children;
@@ -61,6 +63,7 @@ public:
 
 class Trie
 {
+private:
     TrieNode *root;
 
 public:
@@ -72,21 +75,23 @@ public:
         root = new TrieNode('\0');
     }
 
-    bool insertWord(TrieNode *root, string word) // d i m 
+    bool insertWord(TrieNode *root, string word)
     {
-        if (word.size() == 0)
+        // Base Case -->
+        if (word.length() == 0)
         {
-            if (!root->isTerminal)
+            if (root->isTerminal == true)
+            {
+                return false;
+            }
+            else
             {
                 root->isTerminal = true;
                 return true;
             }
-            else
-            {
-                return false;
-            }
         }
 
+        // Small Calculation -->
         int index = word[0] - 'a';
         TrieNode *child;
         if (root->children[index] != NULL)
@@ -99,41 +104,46 @@ public:
             root->children[index] = child;
         }
 
+        // Recursive Call -->
         return insertWord(child, word.substr(1));
     }
 
     void insertWord(string word)
     {
-        if (insertWord(root, word)) // if this returns true then count increases -->
+        if (insertWord(root, word))
         {
             this->count++;
         }
     }
 
+    // search Pattern helper
     bool searchPattern(TrieNode *root, string word)
     {
+        // Base Case -->
         if (word.size() == 0)
         {
             return true;
         }
+        // small Calculation -->
         if (root->children[word[0] - 'a'] == NULL)
         {
             return false;
         }
+        // recursive call -->
         return searchPattern(root->children[word[0] - 'a'], word.substr(1));
     }
-
+    // Main Search Function
     bool searchPattern(string word)
     {
         return searchPattern(root, word);
     }
 
-    bool patternMatching(vector<string> vect, string pattern)
+    bool PatternMatching(vector<string> vect, string pattern)
     {
         for (int i = 0; i < vect.size(); i++)
         {
-            string word = vect[i];
-            for (int j = 0; j < word.size(); j++)
+            string word = vect[i]; 
+            for (int j = 0; j < word.size(); j++)// (dimple) --> inserted in 1st iteration . then d (imple) is inserted in 2nd iteration. then di (mple) is inserted in 3 rd iteration --> and so on  -->
             {
                 insertWord(word.substr(j));
             }
@@ -142,24 +152,28 @@ public:
     }
 };
 
-int main()
-{
+int main(){
     Trie t;
+
     int n;
-    cin >> n;
+    cin>>n;
+
     string pattern;
     vector<string> vect;
 
-    for (int i = 0; i < n; ++i)
-    {
+    for(int i = 0 ;i < n ;i++){
         string word;
-        cin >> word;
+        cin>> word;
+
         vect.push_back(word);
     }
-    cin >> pattern;
+    cin>> pattern;
 
-    cout << (t.patternMatching(vect, pattern) ? "true" : "false");
+    cout<<(t.PatternMatching(vect,pattern) ? "true" : " false");
+
+    return 0;
 }
+
 
 // PRACTICE -->
 
