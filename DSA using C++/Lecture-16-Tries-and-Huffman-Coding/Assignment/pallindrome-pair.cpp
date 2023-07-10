15240d4e57510ca03b38081863206793543400a2
sr.chandrashekar <chandru007.raju@gmail.com>
2023-07-10 19:00:57 +0530

    Updated Changes

--- DSA using C++/Lecture-16-Tries-and-Huffman-Coding/Assignment/pallindrome-pair.cpp
+++ DSA using C++/Lecture-16-Tries-and-Huffman-Coding/Assignment/pallindrome-pair.cpp
@@ -1,4 +1,42 @@
+/*
+Palindrome Pair
+Given 'n' number of words, you need to find if there exist any two words which can be joined to make a palindrome or any word, which itself is a palindrome.
+The function should return either true or false. You don't have to print anything.
+
+Input Format :
+The first line of the test case contains an integer value denoting 'n'.
+
+The following contains 'n' number of words each separated by a single space.
+
+Output Format :
+The first and only line of output contains true if the conditions described in the task are met and false otherwise.
+
+Constraints:
+0 <= n <= 10^5
+Time Limit: 1 sec
+
+Sample Input 1 :
+4
+abc def ghi cba
+Sample Output 1 :
+true
+Explanation of Sample Input 1:
+"abc" and "cba" forms a palindrome
+
+Sample Input 2 :
+2
+abc def
+Sample Output 2 :
+false
+Explanation of Sample Input 2:
+Neither their exists a pair which forms a palindrome, nor any of the words is a palindrome in itself. Hence, the output is 'false.'
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
 // Palindrome Pair
+
 class TrieNode {
    public:
     char data;
@@ -100,7 +138,8 @@ class Trie {
         // Write your code here
         return search(root,word);
     }
-    /*..................... Palindrome Pair................... */
+
+    //..................... Palindrome Pair................... 
 
     bool isPalindromePair(vector<string> words) {
         // Write your code here
@@ -119,3 +158,18 @@ class Trie {
         
     }
 };
+
+int main()
+{
+    Trie t;
+    int n;
+    cin >> n;
+    vector<string> words(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> words[i];
+    }
+
+    cout << (t.isPalindromePair(words) ? "true" : "false");
+}
