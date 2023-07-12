8b5005f8d0cb6bd74540e9e5573f7464c44da08d
sr.chandrashekar <chandru007.raju@gmail.com>
2023-07-12 11:07:49 +0530

    Lcs-memoization approach

--- DSA using C++/Lecture-18 DP-2/Code/lcs-memoization.cpp
+++ DSA using C++/Lecture-18 DP-2/Code/lcs-memoization.cpp
@@ -53,7 +53,55 @@ int main(){
 /*...................PRACTICE......................*/
 
 //  Memoization -->
-int lcs(string s,string t){
+/*
+int helper(int ** output,string s, string t){
 
-    helper
+    int m = s.size();
+    int n = t.size();
+    
+    // Base case -->
+    if(s.size() == 0 || t.size() == 0){
+        return 0;
+    }
+
+    // Check if ans already present -->
+    if(output[m][n] != -1){
+        return output[m][n];
+    }
+
+    //Recursive calls
+    // if the first char matches in both the strings --->
+    if(s[0] == t[0]){
+        int ans = 1 + helper(output,s.substr(1),t.substr(1));
+        return ans;
+    }
+    else{
+        int x = helper(output,s.substr(1),t);
+        int y = helper(output,s,t.substr(1));
+    }
+    
+    //Small answer -->
+    int ans = max(x,y);
+
+    //Save the Answer for future Use -->
+    output[m][n] = ans;
+
+    return ans;    
+
 }
+
+int lcs(string s,string t){
+    int m = s.size();
+    int n = t.size();
+
+    int ** output = new int *[m+1];
+    for(int i = 0; i < m+1 ;i++){
+        output[i] = new int[n+1];
+        for(int j = 0; j < n+1 ;j++){
+            output[i][j] = -1;
+        }
+    }
+
+    helper(output,s,t);
+}
+*/
